5daf823eb8a6d7c728f9bc6c50ebf99d13a26dbc
foxmorg@gmail.com <ospytsia@slice.com>
2021-02-14 20:52:55 +0200

    try to put some data to ES using ES client

--- kafka-consumer-elasticsearch/src/main/java/com/github/foxmorg/kafka/tutorial3/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/com/github/foxmorg/kafka/tutorial3/ElasticSearchConsumer.java
@@ -1,4 +1,66 @@
 package com.github.foxmorg.kafka.tutorial3;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class ElasticSearchConsumer {
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+
+        RestHighLevelClient client = createClient();
+
+        String jsonString = "{\"foo\": \"bar\"}";
+
+        IndexRequest indexRequest = new IndexRequest(
+                "twitter",
+                "tweets"
+        ).source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        String id = indexResponse.getId();
+        logger.info(id);
+
+        //close the client gracefully
+        client.close();
+    }
+
+    public static RestHighLevelClient createClient() {
+
+        // replace with your own credentials
+        String hostname = "kafka-course-8409627539.us-east-1.bonsaisearch.net";
+        String username = "2t1ldf4nm9";
+        String password = "f1u4qii2p8";
+
+        // don't do if you run a local ES
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(
+                AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password)
+        );
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+
+        return new RestHighLevelClient(builder);
+    }
 }
